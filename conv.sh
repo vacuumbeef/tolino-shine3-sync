acaa3e8c626ce16cf0021013d7f8002b7b26766f
vacuumbeef <vacuum@beef>
2025-02-04 19:53:02 +0300

    initial commit

---
+++ conv.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Made for tolino shine 3
+#
+# Dependencies:
+# adb
+# adb-sync - https://github.com/google/adb-sync
+# fbconverter - https://github.com/tomneko/fbconverter
+
+books_dir=./Books
+orig_files_dir=./.original_files
+
+mkdir -p $books_dir $orig_files_dir
+
+convert_and_move() {
+	fb2c convert --to epub "$file" $books_dir && mv "$file" ./.original_files
+}
+
+sync() {
+	adb-sync -d $books_dir/ /sdcard/Books
+}
+
+force_update_library() {
+	adb shell "su -c 'am start -n de.telekom.epub/.ui.activities.UsbStorageActivity'"
+	sleep 5
+	adb shell "su -c 'am start -n de.telekom.epub/.ui.activities.UsbStorageActivity'"
+}
+
+
+for file in "$books_dir"/*
+do
+	ext="${file##*.}"
+
+	case $ext in
+		fb2)
+			echo "$file"
+			convert_and_move
+			;;
+		*)
+			;;
+	esac
+done
+
+adb connect 192.168.1.142
+sync
+force_update_library
+adb disconnect 192.168.1.142
